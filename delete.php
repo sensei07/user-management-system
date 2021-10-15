d548caaa751604fc825368588a4bab1489dde84e
Andrii Tomkiv <51452880+sensei07@users.noreply.github.com>
2021-10-15 21:51:43 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,11 +5,37 @@ if (isset($_POST['deleteId'])) {
     $unique = $_POST['deleteId'];
     $sql = "DELETE FROM users WHERE id = $unique";
     $result = mysqli_query($con, $sql);
+    $errorHttp = array(
+        'code' => http_response_code(),
+        'message' => $error
+    );
+    if ($result === true) {
+        $errorHttp = null;
+    }
+    $response = array(
+        'status' => $result,
+        'error' => $errorHttp,
+        'id' => $unique
+    );
+    echo json_encode($response);
 }
 
 if (isset($_POST["selectedId"])) {
     foreach ($_POST["selectedId"] as $id) {
-        $sql = "delete from `users` where id=$id";
+        $sql = "delete from `users` where id='" . $id . "'";
         $result = mysqli_query($con, $sql);
     }
+    $errorHttp = array(
+        'code' => http_response_code(),
+        'message' => $error
+    );
+    if ($result === true) {
+        $errorHttp = null;
+    }
+    $response = array(
+        'status' => $result,
+        'error' => $errorHttp,
+        'selectId' => $_POST['selectedId'],
+    );
+    echo json_encode($response);
 }
