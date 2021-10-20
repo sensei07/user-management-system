666e795f0c85b7f64a667a0ae58c7e7433f561b1
Andrii Tomkiv <51452880+sensei07@users.noreply.github.com>
2021-10-20 15:46:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ $(document).ready(function () {
             paras.remove();
         }
     };
+    // remove checkbox
+    function removeCheckbox() {
+        $('#check_all, .check').prop('checked', false)
+    }
+    /// remove error block
     $('#firstname,#lastname,#role').change(function () {
         let inputName = $('#firstname').val();
         let inputNameLength = inputName.length;
@@ -20,10 +25,6 @@ $(document).ready(function () {
             $('.danger-name').remove();
         }
     })
-    // remove checkbox
-    function removeCheckbox() {
-        $('#check_all, .check').prop('checked', false)
-    }
     /////    CHECKBOX
     $(document).on('click', '#check_all', function () {
         if (this.checked) {
@@ -107,7 +108,7 @@ $(document).ready(function () {
                 if (statusParse === 'on') {
                     statusImg = "<input class='status-input' type='hidden' value='on'><span tooltip='online'><i class='fas fa-circle online'></i></span>";
                 } else {
-                    statusImg = "<input class='status-input' type='hidden' value='on'><span tooltip='offline'><i class='fas fa-circle offline'></i></span>";
+                    statusImg = "<input class='status-input' type='hidden' value='off'><span tooltip='offline'><i class='fas fa-circle offline'></i></span>";
                 }
                 $('#firstname').val('');
                 $('#lastname').val('');
@@ -213,6 +214,9 @@ $(document).ready(function () {
                     roleEdit: roleEdit,
                     idEdit: idEdit,
                 },
+                beforeSend: function () {
+                    idEdit = 0;
+                },
                 success: function (data) {
                     $('#edit-user__btn').remove();
                     let dataParse = $.parseJSON(data);
@@ -235,7 +239,6 @@ $(document).ready(function () {
                     }
                     idEdit = null;
                 }, error: function (data) {
-                    console.log(data);
                     let dataParse = $.parseJSON(data.responseText);
                     let errorText = dataParse.error.message;
                     let confirmError = `<div class="alert alert-danger danger error-name" role="alert">
@@ -254,33 +257,39 @@ $(document).ready(function () {
         $('#deleteModal').modal('toggle');
         $('.modal-title').text('Delete');
         $('.body-modal').text('Are you sure want to delete this user?');
-        let btnDelete = `<button type='button' class='btn btn-dark' id='delete-row'>OK</button>`;
-        if (!$('#deleteModal .modal-footer').children('#delete-row').length > 0) {
-            $('#deleteModal .modal-footer').append(btnDelete);
+        if ($("#delete-row").length === 0) {
+            $('#deleteModal .modal-footer').append("<button type='button' class='btn btn-dark' id='delete-row'>OK</button>");
         }
         $('#delete-row').click(function () {
             $.ajax({
                 url: "delete.php",
-                cache: false,
                 type: 'post',
                 data: {
                     deleteId: deleteId
                 },
                 dataType: 'html',
                 success: function (data) {
+                    $('#delete-row').remove();
                     dataParse = $.parseJSON(data);
                     $('#deleteModal').modal('hide');
                     $(`[data-idrow='${dataParse.id}']`).remove();
+                    deleteId = 0;
                 }
             })
         });
     });
     ///// Edit status
-    $('.btn-okey:first').click(function () {
+    $(document).on('click', '.btn-okey:first', function () {
         let id = [];
+        let idString = '';
         $('.check:checkbox:checked').each(function (i) {
-            id[i] = $(this).val();
+            id.push($(this).val());
+            idString = id.toString();
         })
+        $("#deleteModal").on("hidden.bs.modal", function () {
+            console.log(idString);
+            console.log(id);
+        });
         // select's confirm window
         if ($('.select:first').val() === '0' || id.length === 0) {
             $('#deleteModal').modal('show');
@@ -289,8 +298,8 @@ $(document).ready(function () {
             $('#delete-row').remove();
         }
         // actions 1/2/3
-        if ($('.select:first').val() === '1') {
-            let selectValue = 'on';;
+        if ($('.select:first').val() === '1' && id.length >= 1) {
+            let selectValue = 'on';
             $.ajax({
                 url: 'update.php',
                 method: 'POST',
@@ -308,7 +317,7 @@ $(document).ready(function () {
                     }
                 }
             });
-        } else if ($('.select:first').val() === '2') {
+        } else if ($('.select:first').val() === '2' && id.length >= 1) {
             let selectValue = 'off';;
             $.ajax({
                 url: 'update.php',
@@ -345,21 +354,35 @@ $(document).ready(function () {
                 $.ajax({
                     url: 'delete.php',
                     method: 'POST',
-                    data: { selectedId: id },
+                    data: {
+                        selectedId: idString
+                    },
                     success: function (data) {
                         $('#delete-row').remove();
                         dataParse = $.parseJSON(data);
-                        let dataId = dataParse.selectId;
+                        let res = dataParse.selectId.split(",");
+                        console.log(res);
+                        // let dataId = dataParse.selectId;
                         $('#deleteModal').modal('hide');
                         removeCheckbox();
-                        for (let i = 0; i < dataId.length; i++) {
-                            $(`[data-idrow='${dataId[i]}']`).remove();
+                        for (let i = 0; i <= res.length; i++) {
+                            $(`[data-idrow='${res[i]}']`).remove();
                         };
                     },
                 });
             });
         };
     });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     //// second block
     $('.btn-okey:last').click(function () {
         let id = [];
@@ -374,7 +397,7 @@ $(document).ready(function () {
             $('#delete-row').remove();
         }
         // actions 1/2/3
-        if ($('.select:last').val() === '1') {
+        if ($('.select:last').val() === '1' && id.length >= 1) {
             let selectValue = 'on';
             $.ajax({
                 url: 'update.php',
@@ -393,7 +416,7 @@ $(document).ready(function () {
                     }
                 }
             });
-        } else if ($('.select:last').val() === '2') {
+        } else if ($('.select:last').val() === '2' && id.length >= 1) {
             let selectValue = 'off';;
             $.ajax({
                 url: 'update.php',
