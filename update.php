03b80343b1335a544f7840f98e6a62f9df32771c
Andrii Tomkiv <51452880+sensei07@users.noreply.github.com>
2021-10-21 01:25:04 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,25 +1,57 @@
 <?php
 include 'connect.php';
 
-if (isset($_POST["id"]) and isset($_POST['selectValue'])) {
-    $unique = $_POST['id'];
-    $selectValue = $_POST['selectValue'];
-    foreach ($_POST["id"] as $unique) {
-        $sql = "UPDATE users SET status='$selectValue' WHERE id = $unique";
-        $result = mysqli_query($con, $sql);
+if (isset($_POST["id"]) and isset($_POST['action']) and isset($_POST['selectValue'])) {
+    if ($_POST['action'] == 'update-status') {
+        $selectValue = $_POST['selectValue'];
+        $arr = explode(',', $_POST['id']);
+        foreach ($arr as $id) {
+            $sql = "UPDATE users SET status='$selectValue' WHERE id = $id";
+            $result = mysqli_query($con, $sql);
+        }
+        $errorHttp = array(
+            'code' => http_response_code(),
+            'message' => $error
+        );
+        if ($result === true) {
+            $errorHttp = null;
+        } else {
+            return;
+        }
+        $userStatus = '';
+        if ($selectValue === 'on') {
+            $userStatus = 'online';
+        } else {
+            $userStatus = 'offline';
+        }
+        $response = array(
+            'status' => $result,
+            'error' => $errorHttp,
+            'selectValue' => $selectValue,
+            'selectId' => $_POST['id'],
+            'userStatus' => $userStatus,
+            'action' => $_POST['action']
+        );
+        echo json_encode($response);
+    } elseif ($_POST['action'] == 'delete') {
+        $arr = explode(',', $_POST['id']);
+        foreach ($arr as $unique) {
+            $sql = "DELETE from `users` WHERE id = $unique";
+            $result = mysqli_query($con, $sql);
+        }
+        $errorHttp = array(
+            'code' => http_response_code(),
+            'message' => $error
+        );
+        if ($result === true) {
+            $errorHttp = null;
+        }
+        $response = array(
+            'status' => $result,
+            'error' => $errorHttp,
+            'selectId' => $_POST['id'],
+            'action' => $_POST['action'],
+        );
+        echo json_encode($response);
     }
-    $errorHttp = array(
-        'code' => http_response_code(),
-        'message' => $error
-    );
-    if ($result === true) {
-        $errorHttp = null;
-    }
-    $response = array(
-        'status' => $result,
-        'error' => $errorHttp,
-        'selectValue' => $selectValue,
-        'selectId' => $_POST['id'],
-    );
-    echo json_encode($response);
 }
