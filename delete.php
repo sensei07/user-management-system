38a44489f157f12551467224c86d73bc94e9c0de
Andrii Tomkiv <51452880+sensei07@users.noreply.github.com>
2021-10-21 01:25:34 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -20,23 +20,23 @@ if (isset($_POST['deleteId'])) {
     echo json_encode($response);
 }
 
-if (isset($_POST["selectedId"])) {
-    $arr = explode(',', $_POST['selectedId']);
-    foreach ($arr as $unique) {
-        $sql = "DELETE from `users` WHERE id = $unique";
-        $result = mysqli_query($con, $sql);
-    }
-    $errorHttp = array(
-        'code' => http_response_code(),
-        'message' => $error
-    );
-    if ($result === true) {
-        $errorHttp = null;
-    }
-    $response = array(
-        'status' => $result,
-        'error' => $errorHttp,
-        'selectId' => $_POST['selectedId'],
-    );
-    echo json_encode($response);
-}
+// if (isset($_POST["selectedId"])) {
+//     $arr = explode(',', $_POST['selectedId']);
+//     foreach ($arr as $unique) {
+//         $sql = "DELETE from `users` WHERE id = $unique";
+//         $result = mysqli_query($con, $sql);
+//     }
+//     $errorHttp = array(
+//         'code' => http_response_code(),
+//         'message' => $error
+//     );
+//     if ($result === true) {
+//         $errorHttp = null;
+//     }
+//     $response = array(
+//         'status' => $result,
+//         'error' => $errorHttp,
+//         'selectId' => $_POST['selectedId'],
+//     );
+//     echo json_encode($response);
+// }
